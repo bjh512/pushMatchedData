657a5feac2f5489bc4454fa9fcd6c8e0057dea21
citron0137 <37734231+citron0137@users.noreply.github.com>
2018-12-07 15:31:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ import requests
 import sys
 import json
 
-def match_insert(original_movie_seq,sub_movie_seq):
-    body = {"sub_file_seq": sub_movie_seq, "original_movie_seq": original_movie_seq, "search_date":"2018-11-01T12:00:00"}
+def match_insert(original_movie_seq,sub_movie_seq, search_date):
+    body = {"sub_file_seq": sub_movie_seq, "original_movie_seq": original_movie_seq, "search_date":search_date}
     headers={"Content-type" : "application/json"}
     r = requests.post("http://13.209.99.61:8080/detect_hit", data=json.dumps(body), headers = headers)
     r.json()
@@ -68,7 +68,7 @@ def upload_match_result(original_movie_seq,sub_movie_seq, match_detail_list):
     match_seq = match_table_dto["seq"]
     for eachImg in match_detail_list:
         sub_img_dto = upload_sub_img(eachImg["sub_img_path"],sub_movie_seq)
-        upload_match_detail(match_seq, eachImg["original_img_seq"], sub_img_dto["seq"], eachImg["compare_hash"])
+        upload_match_detail(match_seq, eachImg["original_img_seq"], sub_img_dto["seq"], eachImg["compare_hash"],"2018-11-01T12:00:00")##TODO NOW로  
          
 
 #### main Stream Start
